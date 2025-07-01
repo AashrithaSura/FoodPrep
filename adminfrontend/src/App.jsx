a7e787eca7a627b93f8ed45099f7d89ef3667e16
AashrithaSura <surak638@gmail.com>
2025-07-01 13:04:35 +0530

    Url updated

--- adminfrontend/src/App.jsx
+++ adminfrontend/src/App.jsx
@@ -12,7 +12,7 @@ import Logout from './components/Logout/Logout';
 import './App.css';
 import { StoreContext } from './context/StoreContext'; 
 
-const url = 'http://localhost:5173/';
+const url = 'hhttps://food-prep-ewf4.onrender.com//';
 
 const App = () => {
   const { loginPopup } = useContext(StoreContext); 
--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 const allowedOrigins = [
-  'http://localhost:5173', // User frontend
+  'hhttps://food-prep-ewf4.onrender.com/', // User frontend
   'http://localhost:5174', // Admin frontend
 
 ];
