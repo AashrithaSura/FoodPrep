550fd3f9976a5a534083c131de00f16f6257277e
AashrithaSura <surak638@gmail.com>
2025-07-01 15:17:47 +0530

    Fixed

--- adminfrontend/src/screens/Add/Add.jsx
+++ adminfrontend/src/screens/Add/Add.jsx
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 import { StoreContext } from '../../context/StoreContext';
 import './Add.css';
 
-const Add = ({ url = "http://localhost:4000" }) => {
+const Add = ({ url = "https://foodprepbackend-53br.onrender.com" }) => {
   const { admin, setLoginPopup } = useContext(StoreContext); 
 
   const [image, setImage] = useState(false);
